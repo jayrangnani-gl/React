754c84e9f008815caad1a12508026e7bd2c64766
jayrangnani-gl <142883278+jayrangnani-gl@users.noreply.github.com>
2024-12-01 12:49:35 +0530

    first commit

---
+++ src/components/ProductCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ProductCard.css';
+
+const ProductCard = ({ product }) => {
+
+    const { name, price, image } = product;
+
+    return (
+        <div className="productCard">
+        <img src={image} alt={name} />
+        <p className="name">{name}</p>
+        <div className="action">
+          <p>${price}</p>
+          <button>Add To Cart</button>
+        </div>
+      </div>
+    );
+};
+
+export default ProductCard;
